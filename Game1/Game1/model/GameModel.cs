1f6308c49232cb73cbdbc5425e2c361cb394decc
sahib <sahib@hotmail.se>
2014-11-06 19:13:18 +0100

    Laboration 1 (uppgift 1,2 och 3)

---
+++ Game1/Game1/model/GameModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game1.model
+{
+    class GameModel
+    {
+        float LogicalX = 6;
+        float LogicalY = 0;
+        
+
+        internal float GetLogicalX()
+        {
+            return LogicalX;
+        }
+
+        internal float GetLogicalY()
+        {
+            return LogicalY;
+        }
+
+    }
+}
---
+++ Game1/Game1/view/Camera.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Game1.model;
+using Microsoft.Xna.Framework;
+
+namespace Game1.view
+{
+    class Camera
+    {
+        private const int sizeOfTile = 64;
+        private const int borderSize = 64;
+        private model.GameModel model;
+        private static int frame = 640;
+        private float scale;
+        private int levelWidth;
+        private int levelHeight;
+        private int width;
+        private int height;
+
+
+        public Camera(int levelWidth, int levelHeight)
+        {
+            this.model = new GameModel();
+            this.levelWidth = levelWidth;
+            this.levelHeight = levelHeight;
+        }
+
+        internal void RenderWhite()
+        {
+            float logicalX = this.model.GetLogicalX();
+            float logicalY = this.model.GetLogicalY();
+
+
+            float visualX = borderSize + logicalX * sizeOfTile;
+            float visualY = borderSize + logicalY * sizeOfTile;
+
+            //Resultat : uppgift 1 
+
+            //Logisk koordinat	Visuell koordinat
+
+            //0,0	            64, 64
+            //7,0	            512, 64
+            //1,7	            128, 512
+            //7,7	            512, 512
+
+        }
+
+        
+
+        internal void RenderBlack()
+        {
+            float logicalX = this.model.GetLogicalX();
+            float logicalY = this.model.GetLogicalY();
+
+            float visualX = borderSize + (7 - logicalX) * sizeOfTile;
+            float visualY = borderSize + (7 - logicalY) * sizeOfTile;
+
+
+            //Resultat : uppgift 2
+
+            //Logisk koordinat	Visuell koordinat
+
+            //0,0	            512 , 512
+            //6,0	            128 , 512
+            //2,7	            384 , 64
+            //7,7	            64 , 64
+
+        }
+
+        internal void setDimensions(int width, int height)
+        {
+            //throw new NotImplementedException();
+
+            this.width = width;
+            this.height = height;
+
+            int scaleX = (width - frame * 2) / levelWidth;
+            int scaleY = (height - frame * 2) / levelHeight;
+
+            scale = scaleX;
+            if (scaleY < scaleX)
+            {
+                scale = scaleY;
+            }
+
+            // resultat för uppgift 3 är -130.
+        }
+
+
+        public float GetScale()
+        {
+            return scale;
+        }
+
+
+    }
+}
